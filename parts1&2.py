0b6a27fd66186218000962fb20f103c969c4115f
BuildTools <unconfigured@null.spigotmc.org>
2020-09-29 13:03:56 -0400

    Commit by Cam on September 29, 2020 13:03:56

--- parts1&2.py
+++ parts1&2.py
@@ -11,7 +11,7 @@ import numpy as np  # For MATHS
 data = np.genfromtxt('gen_data.csv', delimiter=',') #ndarray from numpy
 
 # Print out the shape of the imported data
-print("Shape of imported data: " + str(data.shape))
+#print("Shape of imported data: " + str(data.shape))
 
 # Creating plot
 fig1 = plt.figure(figsize=(200, 3))
@@ -24,24 +24,48 @@ r = data[:, 0].size  # r = 200
 
 # Find the mean of the data
 meanOfData = np.mean(data, axis = 0)
-print("Mean of the data: " + str(meanOfData))
+#print("Mean of the data: " + str(meanOfData))
 
 # Demean the data
 demeanedData = [np.asarray(data) - np.asarray(meanOfData)]  # Yields an array of shape (1, 200, 3)
 demeanedDataProper = np.squeeze(np.asarray(demeanedData))  # Gets rid of the third dimension of size 1. New shape is (200, 3)
 
 
-print("Shape of demeaned data: " + str(demeanedDataProper.shape))
+#print("Shape of demeaned data: " + str(demeanedDataProper.shape))
 
 transposedArray = np.transpose(demeanedDataProper)
 
-print("Shape of transposed array: " + str(transposedArray.shape))
+#print("Shape of transposed array: " + str(transposedArray.shape))
 
 cd = (1/(r-1))*(np.dot(transposedArray, demeanedDataProper))
 
-qd, dd = np.linalg.eig(cd)
+# Calculate qd and dd
+dd, qd = np.linalg.eig(cd)
 
-print(qd)
+print("qd: " + str(qd))
+print("dd: " + str(dd))
+
+
+## 2.2 Dimensionality
+
+# Sorting the eigenvalues in descending order
+eig_value = np.diag(dd)
+ind = np.sort(eig_value)
+eig_val_sorted = ind[:, 2]
+
+
+# Sorting the eigenvectors in descending order
+ind2 = np.sort(qd)
+#print(ind2)
+
+
+
+#print("Before: " + str(ind))
+#print("eig_val_sorted: " + str(eig_val_sorted))
+
+
+
+
 
 
 
