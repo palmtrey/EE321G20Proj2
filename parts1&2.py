1db4eff7b1cc73659a4baa36d67a4bba5548d8e4
BuildTools <unconfigured@null.spigotmc.org>
2020-09-29 09:05:58 -0400

    Commit by Cam on September 29, 2020 09:05:58

--- parts1&2.py
+++ parts1&2.py
@@ -1,6 +1,11 @@
-import matplotlib.pyplot as plt # For 2D plotting
-from mpl_toolkits import mplot3d # For 3D plotting
-import numpy as np # For MATHS
+# Title: Parts 1 & 2
+# Purpose: To fulfill parts 1 and 2 of project 2 of EE 321: Systems and Signal Processing
+# Developers: Shawn Boyd, Cameron Palmer, Siddesh Sood
+# Last Modified: September 29th, 2020
+
+import matplotlib.pyplot as plt  # For 2D plotting
+from mpl_toolkits import mplot3d  # For 3D plotting
+import numpy as np  # For MATHS
 
 data = np.genfromtxt('gen_data.csv', delimiter=',') #ndarray from numpy
 
